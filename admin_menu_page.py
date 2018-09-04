2fbd9e5a3add4cda29a0bc5d5bdc727765063e73
Hector_ADS <hramirez@adsmex.com>
2018-09-04 17:59:57 -0500

    Desarrollo completo de la pagina de pin code

--- admin_menu_page.py
+++ admin_menu_page.py
@@ -9,3 +9,27 @@ import checkin_page as checkin
 
 
 class AdminMenuPage(tk.Frame):
+
+    arial40 = '-family Arial -size 40 -weight bold -slant roman '  \
+            '-underline 0 -overstrike 0'
+
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        self.construct_gui()
+
+    def construct_gui(self):
+
+        #Button back
+        img = tk.PhotoImage(file='./assets/arrow.png')
+        btn_back = tk.Button(self, image=img, command=lambda: self.master.switch_frame(checkin.CheckInPage))
+        btn_back.image = img
+        btn_back.place(relx=0.0, rely=0.0, height=78, width=78)
+
+        lbl_title = tk.Label(self, text='Elige una opción:', font=AdminMenuPage.arial40)
+        lbl_title.place(relx=0.17, rely=0.05, height=91, width=484)
+
+        btn_delUser = tk.Button(self, text='Remover\nPersona', font=AdminMenuPage.arial40, background='red', foreground='white', command=lambda: self.master.switch_frame())
+        btn_delUser.place(relx=0.03, rely=0.33, height=250, width=300)
+
+        btn_addUser = tk.Button(self, text='Agregar\nPersona', font=AdminMenuPage.arial40, background='green', command=lambda: self.master.switch_frame())
+        btn_addUser.place(relx=0.54, rely=0.33, height=250, width=300)
--- keypad_page.py
+++ keypad_page.py
@@ -6,12 +6,15 @@ except:
     import tkFont as tkfont    #python 2
 
 import checkin_page as checkin
+import admin_menu_page as admin_page
+import sqlite3
+import hashlib
 
 class KeypadPage(tk.Frame):
 
     keys = [
-        ['1','2','3','4','5'],
-        ['6','7','8','9','0'],
+        ['1','2','3','4','5','<<'],
+        ['6','7','8','9','0','enter'],
     ]
 
     arial20 = '-family Arial -size 20 -weight bold -slant roman'
@@ -19,6 +22,8 @@ class KeypadPage(tk.Frame):
 
     # create global variable for pin
     pin = ''  # empty string
+
+    #sha = hashlib.sha256()
 
     def __init__(self, master):
         tk.Frame.__init__(self, master)
@@ -33,8 +38,12 @@ class KeypadPage(tk.Frame):
         btn_back.image = img
         btn_back.place(relx=0.01, rely=0.01, height=78, width=78)
 
-        self.lbl_pin = tk.Label(self, font=KeypadPage.arial40, text='****', background='white')
-        self.lbl_pin.place(relx=0.27, rely=0.08, height=90, width=314)
+        self.entry_pin = tk.Entry(self, font=KeypadPage.arial40, justify='center', show='*')
+        self.entry_pin.place(relx=0.27, rely=0.08, height=90, width=314)
+
+        img_error = tk.PhotoImage(file='assets/error.png')
+        self.lblError = tk.Label(self,image=img_error)
+        self.lblError.image = img_error
 
         frame_keypad = tk.Frame(self)
         frame_keypad.place(relx=0.03, rely=0.4, relheight=0.56, relwidth=0.95)
@@ -42,66 +51,47 @@ class KeypadPage(tk.Frame):
         for y, row in enumerate(KeypadPage.keys, 0):
             for x, key in enumerate(row):
                 b = tk.Button(frame_keypad, text=key, font=KeypadPage.arial20, command=lambda val=key:self.code(val))
-                posx = 0.02 + x * 0.16
-                posy = 0.04 + y * 0.49
-                print(str(posx) + '-' + str(posy) + ',' + str(x))
-                b.place(relx=posx, rely=posy, height=90, width=90)
+                b.place(relx=(0.02 + x * 0.16), rely=(0.04 + y * 0.49), height=90, width=90)
+                if key == '<<':
+                    b.configure(background='red')
+                elif key == 'enter':
+                    b.configure(background='green')
 
-        btn_del = tk.Button(frame_keypad, text='<<', font=KeypadPage.arial20, background='red')
-        btn_del.place(relx=0.84, rely=0.04, height=90, width=90)
+    def getPassword(self):
+        conn = sqlite3.connect('./DB/reloj_checador.db')
+        c = conn.cursor()
+        c.execute('SELECT admin_password FROM Configuration WHERE idConfig=1')
+        password = c.fetchone()[0]
+        c.close()
+        conn.close()
+        return password
 
-        btn_del = tk.Button(frame_keypad, text='enter', font=KeypadPage.arial20, background='green')
-        btn_del.place(relx=0.84, rely=0.53, height=90, width=90)
 
-    # #construyendo la interfaz de la ventana
-    # def construct_gui(self):
-    #     '''Metodo para construir la interfaz de la ventana'''
-    #     # Boton de configuraciones
-    #     img = tk.PhotoImage(file='./assets/config.png')
-    #     btn_back = tk.Button(self, image=img, command= lambda: self.master.switch_frame(check.CheckInPage))
-    #     btn_back.image = img
-    #
-    #     #Entry keycode
-    #     self.e = tk.Entry(self)
-    #     self.e.grid(row=1, column=0, columnspan=3, ipady=5)
-    #
-    #     ##Keypad
-    #     for y, row in enumerate(KeypadPage.keys, 1):
-    #         for x, key in enumerate(row):
-    #             # `lambda` inside `for` has to use `val=key:code(val)`
-    #             # instead of direct `code(key)`
-    #             b = tk.Button(self, text=key, command=lambda val=key:self.code(val))
-    #             b.grid(row=y+1, column=x+1, ipadx=10, ipady=10)
-    #
-    #
-    #     # Poniendo los widgets en la ventana
-    #     btn_back.grid(row=0, column=0)
-    #
-    # def code(self, value):
-    #
-    #     if value == '*':
-    #         # remove last number from `pin`
-    #         pin = KeypadPage.pin[:-1]
-    #         # remove all from `entry` and put new `pin`
-    #         self.e.delete('0', 'end')
-    #         self.e.insert('end', pin)
-    #
-    #     elif value == '#':
-    #         # check pin
-    #
-    #         if KeypadPage.pin == "3529":
-    #             print("PIN OK")
-    #         else:
-    #             print("PIN ERROR!", KeypadPage.pin)
-    #             # clear `pin`
-    #             pin = ''
-    #             # clear `entry`
-    #             self.e.delete('0', 'end')
-    #
-    #     else:
-    #         # add number to pin
-    #         KeypadPage.pin += value
-    #         # add number to `entry`
-    #         self.e.insert('end', value)
-    #
-    #     print("Current:", KeypadPage.pin)
+    def code(self, value):
+
+        if value == '<<':
+            # remove last number from `pin`
+            KeypadPage.pin = KeypadPage.pin[:-1]
+            # remove all from `entry` and put new `pin`
+            self.entry_pin.delete('0', 'end')
+            self.entry_pin.insert('end', KeypadPage.pin)
+
+        elif value == 'enter':
+            #get pin from DB
+            password = self.getPassword()
+            # check pin
+            if hashlib.sha256(KeypadPage.pin.encode() + b'ads').hexdigest() == password:
+                self.master.switch_frame(admin_page.AdminMenuPage)
+            else:
+                self.lblError.place(relx=0.75, rely=0.15, height=45, width=45)
+                self.after(2000, self.lblError.place_forget)
+                # clear `pin`
+                KeypadPage.pin = ''
+                # clear `entry`
+                self.entry_pin.delete('0', 'end')
+
+        else:
+            # add number to pin
+            KeypadPage.pin += value
+            # add number to `entry`
+            self.entry_pin.insert('end', value)
